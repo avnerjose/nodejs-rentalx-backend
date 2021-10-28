9f7a6e88328c6d96f2839ff17e1989c3704a029a
avnerjose <avnerj.g.r@gmail.com>
2021-10-28 00:30:17 -0300

    Creating app routes

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from "express";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post("/", (req: Request, res: Response) => {
+  return createSpecificationController.handle(req, res);
+});
+
+export { specificationsRoutes };
